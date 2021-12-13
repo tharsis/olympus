9ca7b315f1cfffc029a0ebdda33c80c4acfad018
Guillermo <guillermo.paoletti@gmail.com>
2021-12-13 18:24:16 -0500

    get all the wallet completed missions

--- backend/src/database/user.py
+++ backend/src/database/user.py
@@ -1,9 +1,21 @@
 from sqlalchemy.orm import Session
+from src.models.completed import CompletedDb
 from src.models.users import UserDb
 
 
 def get_user_by_id(db: Session, id: int):
     res = db.query(UserDb).filter(UserDb.id == id).first()
     if res is not None:
+        return True, res
+    return False, None
+
+
+def get_user_missions_by_wallet(db: Session, wallet: str):
+    res = []
+    user = db.query(UserDb).filter(UserDb.wallet == wallet).first()
+    if user is not None:
+        missions = db.query(CompletedDb).filter(CompletedDb.id_user == user.id).all()
+        for m in missions:
+            res.append(m.id_task)
         return True, res
     return False, res
--- backend/src/endpoints/user.py
+++ backend/src/endpoints/user.py
@@ -1,3 +1,4 @@
+from typing import List
 from typing import Optional
 
 from fastapi import APIRouter
@@ -6,6 +7,7 @@ from pydantic.main import BaseModel
 from sqlalchemy.orm import Session
 from src.database import session_for_request
 from src.database.user import get_user_by_id
+from src.database.user import get_user_missions_by_wallet
 from src.endpoints.constants import USER_TAG
 from src.models.users import UserSchema
 
@@ -21,6 +23,10 @@ class UserResponse(BaseModel):
     user: Optional[UserSchema]
 
 
+class MissionsResponse(BaseModel):
+    missions: List[int]
+
+
 @router.get('/user/{user_id}', tags=[USER_TAG], response_model=UserResponse)
 async def get_user(user_id: int, db: Session = Depends(session_for_request)):
     res, user = get_user_by_id(db, user_id)
@@ -28,3 +34,11 @@ async def get_user(user_id: int, db: Session = Depends(session_for_request)):
         return {'result': True, 'user': {'totalPoints': user.total_points, 'wallet': user.wallet}}
     else:
         return {'result': False}
+
+
+@router.get('/user_missions/{user_wallet}', tags=[USER_TAG], response_model=MissionsResponse)
+async def get_user_missions(user_wallet: str, db: Session = Depends(session_for_request)):
+    res, missions = get_user_missions_by_wallet(db, user_wallet)
+    if not res:
+        missions = []
+    return {'missions': missions}
