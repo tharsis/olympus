4a68615fd5666ad9a3f76a22e55d8b8c40e0bfa5
Guillermo <guillermo.paoletti@gmail.com>
2021-12-14 18:23:48 -0500

    get_all_completed_tasks endpoint added

--- backend/src/endpoints/tasks.py
+++ backend/src/endpoints/tasks.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from src.database import session_for_request
 from src.database.tasks import get_all_tasks
 from src.endpoints.constants import TASKS_TAG
+from src.models.completed import CompletedDb
 
 router = APIRouter()
 
@@ -27,3 +28,34 @@ async def tasks(db: Session = Depends(session_for_request)):
     for task in get_all_tasks(db):
         ret.append({'id': task.id, 'name': task.name, 'points': task.points})
     return {'tasks': ret}
+
+
+class Completed(BaseModel):
+    wallet: str
+    missions: List[int]
+
+
+class CompletedTasks(BaseModel):
+    completed: List[Completed]
+
+
+@router.get('/get_all_completed_tasks', tags=[TASKS_TAG], response_model=CompletedTasks)
+async def get_all_completed_tasks(db: Session = Depends(session_for_request)):
+    res = []
+    missions = db.query(CompletedDb).order_by(CompletedDb.id_user).all()
+    current_user_id = None
+    current_user_name = None
+    temp = []
+    for m in missions:
+        if m.id_user != current_user_id:
+            if temp != []:
+                res.append({'wallet': current_user_name, 'missions': temp})
+                temp = []
+            current_user_id = m.id_user
+            current_user_name = m.completed.wallet
+            temp.append(m.id_task)
+        else:
+            temp.append(m.id_task)
+    if temp != []:
+        res.append({'wallet': current_user_name, 'missions': temp})
+    return {'completed': res}
